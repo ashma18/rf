efc057dc55409b55aae954d4cc5fc8f9f76ebfa3
ashma <ashma91@gmail.com>
2018-05-03 14:18:23 +1000

    better cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,15 @@
 class Product < ApplicationRecord
     validates_presence_of :title
+    #mount_uploader :image, ImageUploader
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
 
-  #mount_uploader :image, ImageUploader
+    private
+
+    def ensure_not_referenced_by_any_line_item 
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            throw :abort
+        end
+    end
 end
