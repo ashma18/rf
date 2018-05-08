c537584ab53385693a7d4f4294887b8454f64625
ashma <ashma91@gmail.com>
2018-05-08 11:37:02 +1000

    home page edits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+                  
+                    
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   resources :orders
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :line_items
   resources :carts
   resources :products
   resources :charges
-
-  root 'shop#index', as: 'shop_index'
+  resources :shop
+  # root 'shop#index', as: 'shop_index'
   
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
